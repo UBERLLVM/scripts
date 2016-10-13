07adf65dae7e8e97f62dbe51a83ba96482c9d15c
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-10-14 00:10:14 +0200

    Add build script Based upon DragonTC script Cleaned up a lot of stuff we won't use thanks @frap129
    Temporary. Will make this in the form of a makefile soon.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (C) 2015-2016 UBERROMS - UBERLLVM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   You may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+export TOOLCHAIN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+# Check if the desired version is valid
+if [[ "$1" =~ ^[0-9]+(\.[0-9]+)?$ ]] ; then
+    export UBERCLANG_VERSION=$1
+fi;
+
+if [ "$1" == "master" ]; then
+    export UBERCLANG_VERSION="master"
+fi;
+
+./scripts/uberclang
